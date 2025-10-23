3399acc153207851f64532c300f2fd66f605384a
王小尽 <1809315974@qq.com>
2025-10-23 19:33:38 +0800

    更新

---
+++ src/main/java/ctn/ctnapi/util/PayloadUtil.java
@@ -0,0 +1,18 @@
+package ctn.ctnapi.util;
+
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.server.level.ServerPlayer;
+import net.neoforged.neoforge.network.PacketDistributor;
+
+/// 数据包集
+public class PayloadUtil {
+  /// 发送玩家数据包（服务端到客户端）
+  public static void sendToClient(ServerPlayer serverPlayer, CustomPacketPayload customPacketPayload) {
+    PacketDistributor.sendToPlayer(serverPlayer, customPacketPayload);
+  }
+
+  /// 发送玩家数据包（客户端到服务端）
+  public static void sendToServer(CustomPacketPayload customPacketPayload) {
+    PacketDistributor.sendToServer(customPacketPayload);
+  }
+}
